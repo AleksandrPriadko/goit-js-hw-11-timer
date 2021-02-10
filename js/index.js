fb2c6d712056cd6d03996efe1cd8be58cfe2af9b
AleksandrPriadko <68820567+AleksandrPriadko@users.noreply.github.com>
2021-02-10 23:38:41 +0200

    через класс

--- js/index.js
+++ js/index.js
@@ -1,33 +1,49 @@
-const refs = {
-    date: document.querySelector('[data-value="days"]'),
-    hours: document.querySelector('[data-value="hours"]'),
-    mins: document.querySelector('[data-value="mins"]'),
-    secs: document.querySelector('[data-value="secs"]')
+
+class CountdownTimer {
+    createTimer({ selector, targetDate }) {
+        this.selector = selector;
+        this.targetDate = targetDate;
+        this.refs = {
+            days: document.querySelector('[data-value="days"]'),
+            hours: document.querySelector('[data-value="hours"]'),
+            mins: document.querySelector('[data-value="mins"]'),
+            secs: document.querySelector('[data-value="secs"]'),
+            timer: document.querySelector('#timer-1')
+        };
+    }
+
+    timerStart() {
+        setInterval(() => {
+            // const dateStart = this.targetDate;
+            const curentDate = Date.now();
+            const countdownDate = this.targetDate - curentDate;
+            this.dataUpdate(countdownDate);
+        }, 1000);
+    }
+
+    dataUpdate(time) {
+        const days = this.pud(Math.floor(time / (1000 * 60 * 60 * 24)));
+        const hours = this.pud(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        const mins = this.pud(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = this.pud(Math.floor((time % (1000 * 60)) / 1000));
+        this.timerAdd(days, hours, mins, secs);
+    }
+
+    timerAdd(days, hours, mins, secs) {
+        this.refs.days.textContent = `${days}`
+        this.refs.hours.textContent = `${hours}`
+        this.refs.mins.textContent = `${mins}`
+        this.refs.secs.textContent = `${secs}`
+
+    }
+
+    pud(value) {
+        return String(value).padStart(2, "0");
+    }
 }
 
-function timerStart() {
-    const dateStart = Date.now();
-    const targetDate = 604800000;
-    const countdown = dateStart + targetDate;
-    setInterval(() => {
-        const curentDate = Date.now();
-        const countdownDate = countdown - curentDate;
-        dataUpdate(countdownDate);
-    }, 1000);
-    return
-}
-timerStart(dataUpdate);
-
-function dataUpdate(time) {
-    const days = pud(Math.floor(time / (1000 * 60 * 60 * 24)));
-    const hours = pud(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-    const mins = pud(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-    const secs = pud(Math.floor((time % (1000 * 60)) / 1000));
-    refs.date.textContent = `${days}`
-    refs.hours.textContent = `${hours}`
-    refs.mins.textContent = `${mins}`
-    refs.secs.textContent = `${secs}`
-};
-function pud(value) {
-    return String(value).padStart(2, "0");
-};
+const timer = new CountdownTimer({
+    selector: '#timer-1',
+    targetDate: new Date('February 14, 2021'),
+});
+timer.timerStart();
